4f66958c02359b217b417e2a45e6becc5f43ba35
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-02-12 14:44:33 +0900

    GameResult 작업 : 클라이언트에서 받은 정보를 바탕으로 DB 업데이트

---
+++ Controllers/DTO/GameResult.cs
@@ -0,0 +1,26 @@
+﻿// 요청 데이터
+public class GameResultReq
+{
+    // 젤리, 돈, 플레이 시간
+    public string? UserId { get; set; }
+    public TimeSpan? PlayTime  { get; set; }
+    public Dictionary<int/*itemID*/, int/*count*/>? Items { get; set; }
+}
+
+// 응답 데이터
+public class GameResultRes
+{
+    public int MoneyPoint { get; set; }
+    public int Level { get; set; }
+    public int Exp{ get; set; }
+    public int MaxScore{ get; set; }
+}
+
+public class TestUserInfo
+{
+    public int Id { get; set; }
+    public int Level { get; set; }
+    public int Exp { get; set; }
+    public int MoneyPoint { get; set; }
+    public int MaxScore { get; set; }
+}
