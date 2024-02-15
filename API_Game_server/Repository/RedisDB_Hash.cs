d4d137776f768823ee623f6832127f5636b7e7fc
juncheolkim <kimjoon889@gmail.com>
2024-02-15 11:13:46 +0900

    #28 Implement RedisSetMethod

--- API_Game_server/Repository/RedisDB_Hash.cs
+++ API_Game_server/Repository/RedisDB_Hash.cs
@@ -23,19 +23,21 @@ namespace API_Game_Server.Repository
             // 반환값 X
             await _db.HashSetAsync(key, Entries);
         }
-        public async Task<RedisValue[]> GetHash(string key, List<string> Items)
+        public async Task<string[]> GetHash(string key, string[] Items)
         {
-            // string을 요소로 갖는 List를 인자로 받으면,
+            // string 배열을 인자로 받으면,
             // 해당 데이터를 RedisValue[] 자료형으로 변환하여 HashGetAsync에 전달한다.
-            var Entries = new RedisValue[Items.Count];
+            var Entries = new RedisValue[Items.Length];
             int i = 0;
             foreach (var Item in Items)
             {
                 Entries[i++] = new RedisValue(Item);
             }
-            // 반환값 X
-            var result = await _db.HashGetAsync(key,Entries);
-            return result;
+            RedisValue[] result = await _db.HashGetAsync(key,Entries);
+            if (result == null) return null;
+            if (result.Length == 0) return new string[0];
+            // RedisValue[] 자료형을 string[] 으로 변환하여 반환한다.
+            return Array.ConvertAll(result, x => (string)x);
         }
         public async Task<bool> ClearHash(string key)
         {
