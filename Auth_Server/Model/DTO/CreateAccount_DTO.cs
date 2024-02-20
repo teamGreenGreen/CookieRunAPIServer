286bb345b9716e14d37dc6d04a73bcb8a6b896e6
ems789 <ems789@naver.com>
2024-02-20 16:57:01 +0900

    인증 관련 DTO에 유효성 검사 추가

--- Auth_Server/Model/DTO/CreateAccount_DTO.cs
+++ Auth_Server/Model/DTO/CreateAccount_DTO.cs
@@ -1,10 +1,16 @@
 using API_Game_Server.Model.DTO;
+using System.ComponentModel.DataAnnotations;
 
 namespace Auth_Server.Model.DTO;
 
 public class CreateAccountReq
 {
+    [Required]
+    [StringLength(40, ErrorMessage = "Email is To Long"]
+    [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
     public string Email { get; set; }
+    [Required]
+    [StringLength(30, ErrorMessage = "Password is To Long"]
     public string Password { get; set; }
 }
 
