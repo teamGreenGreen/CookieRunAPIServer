7558fd3bf01199635e8cd57249db0ecb9e78e0db
KimChowon <chowon986@gmail.com>
2024-02-22 17:47:57 +0900

    로비 씬에서 플레이어 정보를 가져오기 위한 Post 함수 생성

--- API_Game_server/Services/UserService.cs
+++ API_Game_server/Services/UserService.cs
@@ -1,17 +1,21 @@
 using API_Game_Server.Model.DAO;
+using API_Game_Server.Model.DTO;
 using API_Game_Server.Repository;
 using API_Game_Server.Repository.Interface;
 using API_Game_Server.Services.Interface;
+using System.Text.Json;
 
 namespace API_Game_Server.Services;
 
 public class UserService : IUserService
 {
     readonly IGameDB gameDb;
+    readonly IRedisDB redisDb;
 
-    public UserService(IGameDB gameDb)
+    public UserService(IGameDB gameDb, IRedisDB redisDb)
     {
         this.gameDb = gameDb;
+        this.redisDb = redisDb;
     }
 
     public async Task<(EErrorCode, UserInfo)> GetUserInfo(Int64 userId)
@@ -23,5 +27,25 @@ public class UserService : IUserService
         }
 
         return (EErrorCode.None, userInfo);
+    }
+
+    public async Task<EErrorCode> GetUserInfoBySessionId(string sessionId, UserInfoRes res)
+    {
+        try
+        {
+            // 레디스에서 데이터 가져오기
+            string sessionIdKey = string.Format("user_info:session_id:{0}", sessionId);
+            string redisFindResult = await redisDb.GetString(sessionIdKey);
+
+            res.UserInfo = JsonSerializer.Deserialize<UserInfo>(redisFindResult);
+
+            // 가져온 json 문자열을 userInfo로 역직렬화
+            return EErrorCode.None;
+        }
+        catch
+        {
+            // TODO.김초원 : 로그아웃
+            return EErrorCode.UserService_GetRedisUserInfoFail;
+        }
     }
 }
